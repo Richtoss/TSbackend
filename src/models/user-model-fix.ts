a29e98d730fd828295f47ecdf5a9701986bae473
Richtoss <CoryJRichter@gmail.com>
2024-07-13 16:48:58 -0500

    sync

---
+++ src/models/user-model-fix.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  role: 'employee' | 'manager';
+}
+
+const UserSchema: Schema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['employee', 'manager'], default: 'employee' },
+});
+
+export default mongoose.model<IUser>('User', UserSchema);
